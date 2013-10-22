326bd18d51a43532b71d1cd5d97c76eecbe2f404
Fernando Boaglio <boaglio@gmail.com>
2013-10-21 23:54:25 -0200

    capítulo 2

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,18 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+	public static Result index() {
+		return ok(index.render("Your new application is ready."));
+	}
+
+	public static Result sobre() {
+		// ok("Sobre");
+		return ok(views.html.sobre.render("Top 100 filmes Cult",play.core.PlayVersion.current()));
+	}
 
 }
---
+++ app/controllers/Inicio.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class Inicio extends Controller {
+
+	public static Result index() {
+
+		return ok(views.html.inicio.render());
+	}
+}
